ef65aa8f912f806da7f0caf7cbb748b283aadb5a
Adriano Amorim <adryanoamorim@hotmail.com>
2022-06-30 17:42:54 -0300

    Ajustado busca por nome com modo Insesative (prisma

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient()
 app.use(cors())
 
 
-//PEGA (ID,NOME,URLIMG, NOME CARGOS) DOS MEMBROS PARA LISTAR NA TELA HOME
+//Busca (ID,NOME,URLIMG, NOME CARGOS) DOS MEMBROS PARA LISTAR NA TELA HOME
 app.get('/membros', async (req, res) => {
   const membros = await prisma.membros.findMany({
     select: {
@@ -32,7 +32,8 @@ app.get('/buscar/:nome', async (req, res) => {
   const membros = await prisma.membros.findMany({
     where: {
       nome:{
-        startsWith: nome
+        startsWith: nome,
+        mode: 'insensitive'
       }
     },
     select: {
@@ -52,14 +53,14 @@ app.get('/buscar/:nome', async (req, res) => {
   res.send(membros)
 })
 
-//CARREGA TODOS OS CARGOS EXISTENTES
+//Busca TODOS OS CARGOS EXISTENTES
 app.get('/cargos', async (req, res) => {
   const cargos = await prisma.cargo.findMany();
   res.send(cargos);
 })
 
 
-//PEGA O MEMBRO SELECIONADO PELO ID
+//Busca O MEMBRO SELECIONADO PELO ID
 app.get("/membro/:id", async (req, res) => {
   const { id } = req.params;
   const membro = await prisma.membros.findUnique({
